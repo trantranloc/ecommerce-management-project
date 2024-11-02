294a45ab98f9faab76538761b4a33c0c4fd08bba
Ryon <tran26122003@gmail.com>
2024-11-02 23:13:22 +0700

    add index

---
+++ src/main/java/com/project/ecommerce/controller/admin/WebController.java
@@ -0,0 +1,14 @@
+package com.project.ecommerce.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class WebController {
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+}
